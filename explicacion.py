562fc70c1d2f31bfa2d783378146e70b6d5d0721
Juan Pablo <71467905+juanpaat@users.noreply.github.com>
2026-01-28 16:50:03 -0500

    crear funcion de resta

--- explicacion.py
+++ explicacion.py
@@ -8,3 +8,13 @@ def interact(system_input,system_prompt=P3):
 
     output = ephemer(prompt)
     return output
+
+
+ # crear funcion de resta
+def ObltrtNOapt(q):     #No 1 word? No return cicle - filter
+    brk=0
+    while brk!=1:
+        I=interact(q+" return a word.",B1)
+        brk=len(I.split())
+        #print(q,YEL+"ObltrtNOapt:"+RES,I)  #DEBUG
+    return I
